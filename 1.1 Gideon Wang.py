501ccb43179f008aee4874b6b5f7dfaec77e9905
Gideon-wang <gideonw.25225@obhs.school.nz>
2025-08-26 14:34:37 +1200

    work out the total

--- 1.1 Gideon Wang.py
+++ 1.1 Gideon Wang.py
@@ -1,7 +1,7 @@
 '''
    name=Gideon Wang
-   version:1
-   date:25/08/2025
+   version:2
+   date:26/08/2025
    description:assessment
 '''
 #----------------libraries---------------
@@ -9,12 +9,12 @@
 def times(result):
     for i in range(result):
         days=["day1","day2","day3","day4","day5"]
-        running_result=input("what is your running times in minutes for",days(i),":")
-        i+=1
-        list_result=[]
+        running_result=int(input(f"what is your running times in minutes for {days[i]} :"))
         list_result.append(running_result)
 #----------------main routine------------
 name=input("enter your name:")
 age=int(input("enter you age:"))
-result=5
-times(result)
+list_result=[]
+times(5)
+total=list_result[0]+list_result[1]+list_result[2]+list_result[3]+list_result[4]
+print(total)
